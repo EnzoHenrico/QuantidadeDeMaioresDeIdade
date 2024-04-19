9ad848350676c01d349affe49fffe751d770c8b6
EnzoHenrico <enzo.mascia1@gmail.com>
2024-04-19 15:58:54 -0300

    Add project files.

---
+++ QuantidadeDeMaioresDeIdade/Program.cs
@@ -0,0 +1,24 @@
+﻿// Faça um programa que receba a idade de 10 pessoas e mostre a quantidade de maiores e menores de idade.
+int currentPersonAge = 0, minorCount = 0, majorCount = 0;
+
+do
+{
+    Console.Write($"Diga a idade da pessoa {(minorCount + majorCount) + 1}: ");
+    currentPersonAge =  int.Parse(Console.ReadLine());
+    
+    if (currentPersonAge < 0)
+    {
+        Console.WriteLine("\n-> Idade não pode ser negativa, tente novamente.\n");
+    }
+    else if (currentPersonAge < 18)
+    {
+        minorCount++;
+    }
+    else
+    {
+        majorCount++;
+    }
+}
+while (minorCount + majorCount < 10);
+
+Console.WriteLine($"\nExistem {minorCount} menores de idade e {majorCount} maiores de idade.");
